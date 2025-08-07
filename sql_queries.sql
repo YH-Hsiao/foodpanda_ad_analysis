ec92c4a3c4b341deea4ec426fedf390be9a0e87c
YH-Hsiao <violet870819@gmail.com>
2025-08-07 14:02:43 +0800

    Delete sql_queries.sql

--- sql_queries.sql
+++
@@ -1,8 +0,0 @@
--- 各廣告渠道營收、花費及 ROAS
-SELECT MarketingChannel,
-       SUM(Revenue) AS TotalRevenue,
-       SUM(MarketingCost) AS TotalCost,
-       ROUND(SUM(Revenue)/NULLIF(SUM(MarketingCost),0), 2) AS ROAS
-FROM foodpanda_sales
-GROUP BY MarketingChannel
-ORDER BY ROAS DESC;
